b50724eca2414e6c0b19edb8354c8d5082e70d7d
Gaston Caldeiro <chugas488@gmail.com>
2012-10-26 13:44:22 -0200

    irelevant

--- lib/task/observerTask.class.php
+++ lib/task/observerTask.class.php
@@ -1,23 +1,22 @@
 <?php
 
-class observerTask extends sfBaseTask
-{
-  protected function configure()
-  {
+class observerTask extends sfBaseTask {
+
+  protected function configure() {
     // // add your own arguments here
     // $this->addArguments(array(
     //   new sfCommandArgument('my_arg', sfCommandArgument::REQUIRED, 'My argument'),
     // ));
 
     $this->addOptions(array(
-      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'backend'),
-      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'prod'),
-      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'doctrine'),
-      // add your own options here
+        new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'backend'),
+        new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'prod'),
+        new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'doctrine'),
+            // add your own options here
     ));
 
-    $this->namespace        = 'mdNewsletter';
-    $this->name             = 'observer';
+    $this->namespace = 'mdNewsletter';
+    $this->name = 'observer';
     $this->briefDescription = 'Envia los newsletters que esten agendados';
     $this->detailedDescription = <<<EOF
 The [observer|INFO] task does things.
@@ -27,55 +26,48 @@ Call it with:
 EOF;
   }
 
-  protected function execute($arguments = array(), $options = array())
-  {
-    if (!Md_TaskManager::isTaskLocked(__class__)) 
-    {
-        Md_TaskManager::lockTask(__class__);
+  protected function execute($arguments = array(), $options = array()) {
+    if (!Md_TaskManager::isTaskLocked(__class__)) {
+      Md_TaskManager::lockTask(__class__);
 
-        sfContext::createInstance($this->configuration);
-        // initialize the database connection
-        $databaseManager = new sfDatabaseManager($this->configuration);
-        $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+      sfContext::createInstance($this->configuration);
+      // initialize the database connection
+      $databaseManager = new sfDatabaseManager($this->configuration);
+      $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
 
-        $records = Doctrine::getTable('mdNewsletterQueueSubscriber')->scheduledSend(80); // Hay que testear este valor y ver como se comporta el servidor
-        $ids = array();
-        $statics = array();
-        
-        foreach($records as $record)
-        {
-          try
-          {
-            mdNewsletterLog::log(1,'[NOTICE] Before Send Mail: ' . $record['mdNewsletterSubscriber']['email'], $record['md_queue_id'], $record['md_subscriber_id']);
+      $records = Doctrine::getTable('mdNewsletterQueueSubscriber')->scheduledSend(80); // Hay que testear este valor y ver como se comporta el servidor
+      $ids = array();
+      $statics = array();
 
-            if(mdNewsletterQueue::sendMail($record))
-            {
-              mdNewsletterLog::log(2, '[NOTICE] After Send Mail: ' . $record['mdNewsletterSubscriber']['email'], $record['md_queue_id'], $record['md_subscriber_id']);
-              
-              array_push($ids, $record['id']);
-              
-              $statics[$record['md_queue_id']]++;
-            }
-            else
-            {
-              mdNewsletterLog::log(3, '[ERROR] El E-mail no ha podido enviarse' . $record['mdNewsletterSubscriber']['email'], $record['md_queue_id'], $record['md_subscriber_id']);
-            }
-          }catch(Exception $e){
+      foreach ($records as $record) {
+        try {
+          mdNewsletterLog::log(1, '[NOTICE] Before Send Mail: ' . $record['mdNewsletterSubscriber']['email'], $record['md_queue_id'], $record['md_subscriber_id']);
 
-            mdNewsletterLog::log($e->getCode(), '[ERROR] ' . $e->getMessage() . ' '. $record['mdNewsletterSubscriber']['email'], $record['md_queue_id'], $record['md_subscriber_id']);
+          if (mdNewsletterQueue::sendMail($record)) {
+            mdNewsletterLog::log(2, '[NOTICE] After Send Mail: ' . $record['mdNewsletterSubscriber']['email'], $record['md_queue_id'], $record['md_subscriber_id']);
+
+            array_push($ids, $record['id']);
+
+            $statics[$record['md_queue_id']]++;
+          } else {
+            mdNewsletterLog::log(3, '[ERROR] El E-mail no ha podido enviarse' . $record['mdNewsletterSubscriber']['email'], $record['md_queue_id'], $record['md_subscriber_id']);
           }
+        } catch (Exception $e) {
 
-          sleep(1);
+          mdNewsletterLog::log($e->getCode(), '[ERROR] ' . $e->getMessage() . ' ' . $record['mdNewsletterSubscriber']['email'], $record['md_queue_id'], $record['md_subscriber_id']);
         }
-        
-        // Marcarlo fecha de enviado
-        mdNewsletterQueueSubscriber::updateDates($ids);
-        
-        // Actualizamos estadisticas del envio
-        mdNewsletterQueue::stats($statics);
-        
-        Md_TaskManager::unlockTask(__class__);
-    }    
 
+        sleep(1);
+      }
+
+      // Marcarlo fecha de enviado
+      mdNewsletterQueueSubscriber::updateDates($ids);
+
+      // Actualizamos estadisticas del envio
+      mdNewsletterQueue::stats($statics);
+
+      Md_TaskManager::unlockTask(__class__);
+    }
   }
+
 }
